7b576e0b6e8b420040ae73d3e55be4fe407e4a92
5-07 <muzainamunirsiddiqui@gmail.com>
2024-08-18 00:01:25 +0400

    Done with footer!! changed tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@/sections/Header';
 import { Hero } from '@/sections/Hero';
 import { CallToAction } from '@/sections/CallToAction';
+import { Footer } from '@/sections/Footer';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
   <Header />
   <Hero />
   <CallToAction />
+  <Footer />
   </>
   );
 };
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,7 +10,7 @@ export const Hero = () => {
       <div className="container">
         <div className='md:flex items-center'>
         <div className='md:w-[478px]'>
-          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">tag</div>
+          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Under Dev</div>
           <h1 className="text-5xl md:7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Spidex: Web Scraper</h1>
           <p className="text-xl text-[#010D3E] tracking-tight mt-6">
           Effortlessly extract and organize data from any website, turning raw web content into actionable insights.
